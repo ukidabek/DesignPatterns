0578ef52a89528455a4af3ec6a77367702e99d26
Ukidabek <ukidabek@gmail.com>
2018-05-29 19:26:32 +0200

    - Update

---
+++ Singleton/Singleton.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BaseGameLogic.Singleton
+{
+    public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
+    {
+        public static T Instance { get; protected set; }
+
+        [SerializeField] private bool _dontDestroyOnLoad = false;
+
+        protected virtual void CreateInstance()
+        {
+            if (Instance == null)
+            {
+                Instance = this as T;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+
+            if(Instance != null && _dontDestroyOnLoad)
+            {
+                DontDestroyOnLoad(this.gameObject);
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            CreateInstance();
+        }
+
+        protected virtual void Start() {}
+
+    }
+}
