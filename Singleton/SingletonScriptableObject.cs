4cb2aac851588e2ee13239ffac693681f9266b84
Ukidabek <ukidabek@gmail.com>
2019-04-10 23:22:04 +0200

    Initialize method for SingletonScriptableObject added.

--- Singleton/SingletonScriptableObject.cs
+++ Singleton/SingletonScriptableObject.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 namespace BaseGameLogic.Singleton
 {
-    public class SingletonScriptableObject<T> : ScriptableObject where T : SingletonScriptableObject<T>
+    public abstract class SingletonScriptableObject<T> : ScriptableObject where T : SingletonScriptableObject<T>
     {
         private static T _instance = null;
         public static T Instance
         {
             get
             {
-                if(_instance == null)
-                    _instance = Resources.Load(typeof(T).Name) as T;
+                if (_instance == null)
+                    (_instance = Resources.Load(typeof(T).Name) as T).Initialize();
 
                 return _instance;
             }
         }
+
+        protected abstract void Initialize();
     }
 }
