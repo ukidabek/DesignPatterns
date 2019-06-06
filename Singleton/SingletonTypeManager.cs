8a385310c8cf980d44008b7f48b4e5b00db745e3
Ukidabek <ukidabek@gmail.com>
2019-06-06 20:53:45 +0200

    - Type moved to dedicated file.

--- Singleton/SingletonTypeManager.cs
+++ Singleton/SingletonTypeManager.cs
@@ -1,40 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using UnityEngine;
 
 namespace BaseGameLogic.Singleton
 {
-    [Serializable]
-    public abstract class Type
-    {
-        [SerializeField] private string _name = string.Empty;
-
-        public static bool operator ==(Type a, Type b)
-        {
-            return a._name == b._name;
-        }
-
-        public static bool operator !=(Type a, Type b)
-        {
-            return a._name != b._name;
-        }
-
-        public override bool Equals(object obj)
-        {
-            return (obj as Type) == this;
-        }
-
-        public override int GetHashCode()
-        {
-            return _name.GetHashCode();
-        }
-
-        public static implicit operator string(Type type)
-        {
-            return type._name;
-        }
-    }
 
 
     public abstract class SingletonTypeManager<T> : SingletonScriptableObject<T> where T : SingletonScriptableObject<T>
---
+++ Singleton/Type.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+namespace BaseGameLogic.Singleton
+{
+    [Serializable]
+    public abstract class Type
+    {
+        [SerializeField] private string _name = string.Empty;
+
+        public static bool operator ==(Type a, Type b)
+        {
+            return a._name == b._name;
+        }
+
+        public static bool operator !=(Type a, Type b)
+        {
+            return a._name != b._name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return (obj as Type) == this;
+        }
+
+        public override int GetHashCode()
+        {
+            return _name.GetHashCode();
+        }
+
+        public static implicit operator string(Type type)
+        {
+            return type._name;
+        }
+
+        public override string ToString()
+        {
+            return _name;
+        }
+    }
+}
