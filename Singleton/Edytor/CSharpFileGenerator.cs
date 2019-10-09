0018ac0953331f96e06441958b9f93520816afc5
Ukidabek <ukidabek@gmail.com>
2019-10-09 20:40:26 +0200

    SingletonTypeGenerator
    Editor window for automatic custom type generation added.
    Simple CSharp file generation utility added.

---
+++ Singleton/Edytor/CSharpFileGenerator.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BaseGameLogic.Utilities.Editor
+{
+	public class CSharpFileGenerator
+	{
+		public class Using
+		{
+			public Using(string usingPath)
+			{
+				UsingPath = usingPath;
+			}
+
+			public string UsingPath { get; private set; }
+			public override string ToString()
+			{
+				return string.Format("using {0};\r\n", UsingPath);
+			}
+		}
+		public abstract class Attribute
+		{
+			public abstract string Name { get; }
+			public virtual string Parameters { get => string.Empty; }
+			public override string ToString()
+			{
+				string parameters = string.IsNullOrEmpty(Parameters) ? string.Empty : string.Format("({0})", Parameters);
+				return string.Format("[{0}{1}]\r\n", Name, parameters);
+			}
+		}
+
+		public enum FileType { Class, Struct, Interface }
+		public enum FileAccessModifier { Public, Private, Protected }
+
+		public FileAccessModifier AccessModifier = FileAccessModifier.Public;
+		public FileType Type = FileType.Class;
+
+		public List<Using> Usings = new List<Using>();
+		public List<Attribute> ClassAttributes = new List<Attribute>();
+
+		public string Name = "CSharpFile";
+		public string DerivedFrom = string.Empty;
+
+		private string WriteObjects(IList objects)
+		{
+			string content = string.Empty;
+			foreach (var item in objects)
+				content += item.ToString();
+			return content;
+		}
+
+		private string Generate()
+		{
+			string content = string.Empty;
+
+			content += WriteObjects(Usings);
+			content += "\r\n";
+			content += WriteObjects(ClassAttributes);
+
+			content += string.Format(
+				"{0} {1} {2}",
+				AccessModifier.ToString().ToLower(),
+				Type.ToString().ToString().ToLower(),
+				Name);
+
+			if (!string.IsNullOrEmpty(DerivedFrom))
+				content += string.Format(" : {0}", DerivedFrom);
+
+			content += "\r\n";
+			content += "{\r\n";
+			content += "}\r\n";
+
+			return content;
+		}
+
+		public virtual void Save(string path)
+		{
+			File.WriteAllText(string.Format("{0}/{1}.cs", path, Name), Generate());
+		}
+	}
+}
---
+++ Singleton/Edytor/SingletonTypeGenerator.cs
@@ -0,0 +1,85 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+using BaseGameLogic.Utilities.Editor;
+
+namespace BaseGameLogic.Singleton
+{
+	public class SingletonTypeGenerator : EditorWindow
+	{
+		public class CreateAssetMenu : CSharpFileGenerator.Attribute
+		{
+			public override string Name => "CreateAssetMenu";
+			public override string Parameters => string.Format("fileName = \"{0}.asset\", menuName = \"Custom Type/{0}\"", FileName);
+			public string FileName { get; private set; }
+
+			public CreateAssetMenu(string fileName)
+			{
+				FileName = fileName;
+			}
+		}
+
+		private const string Usings = "using UnityEngine;\r\nusing Mechanic.BaseClasses;\r\n\r\n";
+		private const string Mechanic_Class_Heder = "public class {0} : MechanicWithSettings<{0}Settings>";
+		private const string Create_Asset_Menu = "[CreateAssetMenu(fileName = \"{0}.asset\", menuName = \"Custom Type/{0}\")]";
+
+		private const string Mechanic_Settings_Class_Heder = "[CreateAssetMenu(fileName = \"{0} Settings.asset\", menuName = \"Settings/{0} Settings\")]\r\npublic class {0}Settings : ScriptableObject";
+		private const string Class_Body = "\r\n{\r\n}\r\n";
+
+		private string _name = string.Empty;
+		private string _path = string.Empty;
+
+		[MenuItem("Window/Utilities/TypeGenerator")]
+		static void Init()
+		{
+			SingletonTypeGenerator window = (SingletonTypeGenerator)GetWindow(typeof(SingletonTypeGenerator));
+			window.minSize = window.maxSize = new Vector2(300, 50);
+			window.SelectPath();
+			window.Show();
+		}
+
+		private void SelectPath()
+		{
+			if (Selection.activeObject != null)
+			{
+				_path = AssetDatabase.GetAssetPath(Selection.activeObject);
+				if (!string.IsNullOrEmpty(Path.GetExtension(_path)))
+				{
+					string fileName = Path.GetFileName(_path);
+					_path = _path.Replace("/" + fileName, "");
+				}
+			}
+			else
+				_path = Application.dataPath;
+		}
+
+		private void OnSelectionChange()
+		{
+			SelectPath();
+			Repaint();
+		}
+
+		private void OnGUI()
+		{
+			_name = EditorGUILayout.TextField("Mechanic script name: ", _name);
+			EditorGUILayout.LabelField(string.Format("Path: {0}", _path));
+			if (GUILayout.Button("Generate"))
+				GenerateMechanicScipts();
+		}
+
+		private void GenerateMechanicScipts()
+		{
+			CSharpFileGenerator generator = new CSharpFileGenerator();
+			generator.Usings.Add(new CSharpFileGenerator.Using("UnityEngine"));
+			generator.ClassAttributes.Add(new CreateAssetMenu("Test"));
+			generator.DerivedFrom += typeof(ScriptableObject).Name;
+			generator.Save(Application.dataPath);
+			
+			//string data = Usings + string.Format(Mechanic_Class_Heder, _name) + Class_Body;
+			//File.WriteAllText(string.Format("{0}/{1}.cs", _path, _name), data);
+			//data = Usings + string.Format(Mechanic_Settings_Class_Heder, _name) + Class_Body;
+			//File.WriteAllText(string.Format("{0}/{1}Settings.cs", _path, _name), data);
+			AssetDatabase.Refresh();
+		}
+	}
+}
